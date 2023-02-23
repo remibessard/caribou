a8dc6b4b9cfec1587cc2a8f1febb53f5bfe196b4
remibessard <remi.bessard@digyne.com>
2023-02-23 11:43:56 +0100

    UPDATE: (main and SofaCaribou) CMakeLists.txt in order to use SOFA/SofaPython3 CMake macros instead of those in caribouTools.cmake + FIX: windows unittests exe location at install

---
+++ cmake/environment.cmake
@@ -0,0 +1,38 @@
+## Default build type
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    set(CMAKE_BUILD_TYPE "Release")
+endif()
+
+## Force default install prefix if building out-of-tree
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND NOT "${PROJECT_NAME}" STREQUAL "Sofa")
+    set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install/${PROJECT_NAME}" CACHE PATH "Install path prefix, prepended onto install directories." FORCE)
+endif()
+message(STATUS "Install prefix: ${CMAKE_INSTALL_PREFIX}")
+
+## RPATH
+if(UNIX)
+    # RPATH is a field in ELF binaries that is used as a hint by the system
+    # loader to find needed shared libraries.
+    #
+    # In the build directory, cmake creates binaries with absolute paths in
+    # RPATH.  And by default, it strips RPATH from installed binaries.  Here we
+    # use CMAKE_INSTALL_RPATH to set a relative RPATH.  By doing so, we avoid
+    # the need to play with LD_LIBRARY_PATH to get applications to run.
+
+    # see https://cmake.org/Wiki/CMake_RPATH_handling for $ORIGIN doc
+    set(CMAKE_INSTALL_RPATH
+        "$ORIGIN/../lib"
+        "$$ORIGIN/../lib"
+        )
+
+    if(APPLE)
+        set(CMAKE_MACOSX_RPATH ON)
+        list(APPEND CMAKE_INSTALL_RPATH
+            "@loader_path"
+            "@loader_path/../lib"
+            "@executable_path"
+            "@executable_path/../lib"
+            )
+        set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+    endif()
+endif(UNIX)
